2a98c1bd4d7f3184b457645ce9749323d5e75de9
Matias <matiaslimina@gmail.com>
2026-03-29 16:57:27 -0300

    Creacion de Getters y Setters

--- Programacion 2/TP3_IntroduccionPOO/src/Ejercicio_01/RegistroEstudiante.java
+++ Programacion 2/TP3_IntroduccionPOO/src/Ejercicio_01/RegistroEstudiante.java
@@ -6,4 +6,51 @@ public class RegistroEstudiante {
     private String cursos;
     private double calificacion;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre == null || nombre.isEmpty())
+            System.out.println("El nombre no puede estar vacio");
+        else {
+            this.nombre = nombre;
+        }
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        if (apellido == null || apellido.isEmpty()){
+            System.out.println("El apellido no puede estar vacio");
+        }else{
+            this.apellido = apellido;
+        }
+    }
+
+    public String getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(String cursos) {
+        if (cursos == null || cursos.isEmpty()){
+            System.out.println("Los cursos no pueden estar vacios");
+        }else{
+            this.cursos = cursos;
+        }
+    }
+
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(double calificacion) {
+        if (calificacion < 0 || calificacion > 10){
+            System.out.println("La calificacion debe estar entre 0 y 10");
+        }else{
+            this.calificacion = calificacion;
+        }
+    }
 }
