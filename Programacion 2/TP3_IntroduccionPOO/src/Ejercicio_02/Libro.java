0bb55f9d62e043cdd11dffabd7009b5112fbba42
Matias <matiaslimina@gmail.com>
2026-03-29 18:13:43 -0300

    Creacion de Getters y Setters

--- Programacion 2/TP3_IntroduccionPOO/src/Ejercicio_02/Libro.java
+++ Programacion 2/TP3_IntroduccionPOO/src/Ejercicio_02/Libro.java
@@ -4,4 +4,42 @@ public class Libro {
     private String titulo;
     private String autor;
     private int añoPublicacion;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        if(titulo == null || titulo.trim().isEmpty()){
+            System.out.println("El titulo no puede estar vacio");
+        }else{
+            this.titulo = titulo;
+        }
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        if(autor == null || autor.trim().isEmpty()){
+            System.out.println("El autor no puede estar vacio");
+        }else{
+            this.autor = autor;
+        }
+    }
+
+    public int getAñoPublicacion() {
+        return añoPublicacion;
+    }
+
+    public void setAñoPublicacion(int añoPublicacion) {
+        if(añoPublicacion <= 0){
+            System.out.println("El año de publicacion debe ser positivo");
+        }else if(añoPublicacion > 2026){
+            System.out.println("El año de publicacion no puede ser mayor al actual");
+        }else{
+            this.añoPublicacion = añoPublicacion;
+        }
+    }
 }
