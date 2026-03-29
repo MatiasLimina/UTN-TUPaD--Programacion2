6cc7b29bee6d4e98a5de182d12107829dd79572b
Matias <matiaslimina@gmail.com>
2026-03-29 18:28:06 -0300

    Crear setters y getters

--- Programacion 2/TP3_IntroduccionPOO/src/Ejercicio_02/Mascota.java
+++ Programacion 2/TP3_IntroduccionPOO/src/Ejercicio_02/Mascota.java
@@ -4,4 +4,40 @@ public class Mascota {
     private String nombre;
     private String especie;
     private int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if(nombre == null || nombre.trim().isEmpty()){
+            System.out.println("El nombre no puede estar vacio");
+        }else{
+            this.nombre = nombre;
+        }
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        if(especie == null || especie.trim().isEmpty()){
+            System.out.println("La especie no puede estar vacia");
+        }else{
+            this.especie = especie;
+        }
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if(edad <= 0){
+            System.out.println("La edad debe ser positiva");
+        }else{
+            this.edad = edad;
+        }
+    }
 }
