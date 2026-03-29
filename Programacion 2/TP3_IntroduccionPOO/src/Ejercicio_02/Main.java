9cccf17d55380f516fd0f12fa033653dad878112
Matias <matiaslimina@gmail.com>
2026-03-29 18:17:34 -0300

    Crear prueba de errores

--- Programacion 2/TP3_IntroduccionPOO/src/Ejercicio_02/Main.java
+++ Programacion 2/TP3_IntroduccionPOO/src/Ejercicio_02/Main.java
@@ -3,5 +3,13 @@ package Ejercicio_02;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ejercicio 02");
+        System.out.println("Prueba de Errores:");
+        Libro libroErrores = new Libro();
+        libroErrores.setTitulo("");
+        libroErrores.setAutor(null);
+        libroErrores.setAñoPublicacion(202500);
+        libroErrores.mostrarInfo();
+        libroErrores.modificarAñoPublicacion(2027);
+        libroErrores.mostrarInfo();
     }
 }
