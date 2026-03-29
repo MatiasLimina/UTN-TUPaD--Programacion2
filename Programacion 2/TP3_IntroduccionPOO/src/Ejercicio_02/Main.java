d7cbadc29420afcba4460b5e149a534e704d86cf
Matias <matiaslimina@gmail.com>
2026-03-29 18:33:28 -0300

    Crear prueba de errores y funcionalidad

--- Programacion 2/TP3_IntroduccionPOO/src/Ejercicio_02/Main.java
+++ Programacion 2/TP3_IntroduccionPOO/src/Ejercicio_02/Main.java
@@ -3,5 +3,26 @@ package Ejercicio_02;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ejercicio 02");
+        System.out.println("Prueba de Errores:");
+        Mascota mascotaErrores = new Mascota();
+        mascotaErrores.setNombre("");
+        mascotaErrores.setEspecie(null);
+        mascotaErrores.setEdad(-1);
+        mascotaErrores.mostrarInfo();
+
+        System.out.println("Prueba de Funcionalidad:");
+        Mascota mascota = new Mascota();
+        mascota.setNombre("Firulais");
+        mascota.setEspecie("Perro");
+        mascota.setEdad(5);
+        mascota.mostrarInfo();
+        mascota.cumplirAños();
+        mascota.mostrarInfo();
+        mascota.cumplirAños();
+        mascota.cumplirAños();
+        mascota.cumplirAños();
+        mascota.mostrarInfo();
+        System.out.println("----------------------------------------");
+
     }
 }
