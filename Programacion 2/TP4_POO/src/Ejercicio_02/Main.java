1b618b20d335b970e26d902c9c158c5ad793ec52
Matias <matiaslimina@gmail.com>
2026-04-08 19:18:28 -0300

    Crear pruebas de robustez y funcionamiento en Main

--- Programacion 2/TP4_POO/src/Ejercicio_02/Main.java
+++ Programacion 2/TP4_POO/src/Ejercicio_02/Main.java
@@ -1,4 +1,20 @@
 package Ejercicio_02;
 
 public class Main {
+
+    public static void main(String[] args) {
+        SistemaRegistroLibros libro1 = new SistemaRegistroLibros("El Principito", "Antoine de Saint-Exupéry");
+        libro1.actualizarTitulo("El Principito", "Edicion Especial: ");
+        libro1.actualizarTitulo("");
+        System.out.println(libro1.toString());
+        System.out.println("----------------------------------------");
+        SistemaRegistroLibros libro2 = new SistemaRegistroLibros("Lord of The Mysteries", "Cuttlefish that loves diving","EspecialLibros");
+        System.out.println(libro2.toString());
+        libro2.actualizarTitulo("Circle of Inevitability", "");
+        System.out.println(libro2.toString());
+        libro2.setAutor("");
+        SistemaRegistroLibros.cambiarEditorial("PepitoLibros");
+        System.out.println(libro2.toString());
+        System.out.println(libro1.toString());
+    }
 }
