c3c44d6738fd4838a2e55333f9eb3ca9f7094c90
Matias <matiaslimina@gmail.com>
2026-04-09 19:58:12 -0300

    Crear metodo main con pruebas de robustez y funcionamiento

--- Programacion 2/TP4_POO/src/Ejercicio_05/Main.java
+++ Programacion 2/TP4_POO/src/Ejercicio_05/Main.java
@@ -1,4 +1,22 @@
 package Ejercicio_05;
 
 public class Main {
+    public static void main(String[] args) {
+        BancaSimple banco1 = new BancaSimple("Juan", 1000);
+        System.out.println(banco1.toString());
+        banco1.depositar(500);
+        System.out.println(banco1.toString());
+        banco1.retirar(200);
+        System.out.println(banco1.toString());
+        System.out.println("----------------------------------");
+        BancaSimple banco2 = new BancaSimple("Maria");
+        System.out.println(banco2.toString());
+        banco2.retirar(1000);
+        System.out.println(banco2.toString());
+        BancaSimple.mostrarTotalCuentas();
+        System.out.println("----------------------------------");
+        BancaSimple cuenta3 = new BancaSimple("Pedro",200);
+        BancaSimple cuenta4 = new BancaSimple("Ana",3000);
+        BancaSimple.mostrarTotalCuentas();
+    }
 }
