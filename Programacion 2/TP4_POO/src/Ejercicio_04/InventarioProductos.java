a28b5e2783ebb5227cd6243209795d76b66e7776
Matias <matiaslimina@gmail.com>
2026-04-09 16:04:30 -0300

    Crear atributos InventarioProductos

---
+++ Programacion 2/TP4_POO/src/Ejercicio_04/InventarioProductos.java
@@ -0,0 +1,9 @@
+package Ejercicio_04;
+
+public class InventarioProductos {
+    private String nombre;
+    private double  precioBase;
+    private static double IVA = 0.21;
+
+
+}
---
+++ Programacion 2/TP4_POO/src/Ejercicio_04/Main.java
@@ -0,0 +1,4 @@
+package Ejercicio_04;
+
+public class Main {
+}
