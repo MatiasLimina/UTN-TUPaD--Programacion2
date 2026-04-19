0cf40fac3c40be589b944a92eb60a03b4c85fb87
Matias <matiaslimina@gmail.com>
2026-04-19 18:40:34 -0300

    Crear constructores

--- Programacion 2/TrabajoEnClase_Unidad3/Practica_Parcial/src/EjercicioIntegrador_01/Suscripcion.java
+++ Programacion 2/TrabajoEnClase_Unidad3/Practica_Parcial/src/EjercicioIntegrador_01/Suscripcion.java
@@ -5,7 +5,17 @@ public class Suscripcion {
     private String cliente;
     private int mesesPagados;
     private double tarifaMensual;
-    static int totalSuscripciones;
+    static int totalSuscripciones = 0;
 
+    public Suscripcion(String cliente, int mesesPagados, double tarifaMensual) {
+        this.cliente = cliente;
+        this.mesesPagados = mesesPagados;
+        this.tarifaMensual = tarifaMensual;
+        totalSuscripciones ++;
+        numeroSocio = totalSuscripciones;
+    }
+    public Suscripcion(String cliente){
+        this(cliente,1,25000);
+    }
 
 }
