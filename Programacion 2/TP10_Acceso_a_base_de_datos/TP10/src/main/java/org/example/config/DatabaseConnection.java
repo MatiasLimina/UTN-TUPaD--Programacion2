611eba3791d0c7179d35731f3d2ae446cb48065e
Matias <matiaslimina@gmail.com>
2026-06-08 12:59:32 -0300

    Creacion de config base de datos

---
+++ Programacion 2/TP10_Acceso_a_base_de_datos/TP10/src/main/java/org/example/config/DatabaseConnection.java
@@ -0,0 +1,30 @@
+package org.example.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final HikariConfig config = new HikariConfig();
+    private static final HikariDataSource ds;
+
+    static {
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/tp10");
+        config.setUsername("root");
+        config.setPassword("");
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setIdleTimeout(30000);
+
+        ds = new HikariDataSource(config);
+
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
+}
